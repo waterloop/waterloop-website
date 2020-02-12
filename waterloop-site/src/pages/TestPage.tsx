d73b3e6e6fd78d5701a20ef26d7ca0ae44425c74
jxrin <jerin.raisa11@gmail.com>
2020-02-11 20:39:04 -0500

    updating react app with typescript

---
+++ waterloop-site/src/pages/TestPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+class TestPage extends React.Component {
+  render() {
+    return (
+      <div>
+          <h1>Here's a test page</h1>
+      </div>
+    );
+  }
+}
+
+export default TestPage;
