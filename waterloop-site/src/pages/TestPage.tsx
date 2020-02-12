425efbb5e54eac3e43bfda3c97e778942c3f17a8
jxrin <jerin.raisa11@gmail.com>
2020-02-11 21:16:32 -0500

    added test page and component

--- waterloop-site/src/pages/TestPage.tsx
+++ waterloop-site/src/pages/TestPage.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import TestComponent from "../components/TestComponent"
 
 class TestPage extends React.Component {
   render() {
     return (
       <div>
           <h1>Here's a test page</h1>
+          <TestComponent
+          title = "Test Button"
+          link = "https://google.com"
+          ></TestComponent>
       </div>
     );
   }
