4809003b068bbc7ebf45991d5e231819655ec5b8
Quinn1876 <qhodges0442@gmail.com>
2020-02-14 11:46:16 -0500

    skeleton for components

---
+++ waterloop-site/src/components/past-geese-timeline/image-carosel.component.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ImageCarosel: React.FC = () => {
+  return (
+    <>
+
+    </>
+  )
+}
---
+++ waterloop-site/src/components/past-geese-timeline/past-geese-timeline.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PastGeeseTimeline: React.FC = () => {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default PastGeeseTimeline
