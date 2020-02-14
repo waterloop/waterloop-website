faa1a058930b8e2413e14851d3adebd5dbfa228b
Quinn1876 <qhodges0442@gmail.com>
2020-02-14 18:32:48 -0500

    update on image carosel

--- waterloop-site/src/components/past-geese-timeline/image-carosel.component.tsx
+++ waterloop-site/src/components/past-geese-timeline/image-carosel.component.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
 
-const ImageCarosel: React.FC = () => {
-  return (
-    <>
+interface Props {
+  images: string[]
+}
 
-    </>
+const ImageCarosel: React.FC<Props> = (props) => {
+
+  const [currentIndex, setCurrentIndex] = React.useState<number>(0);
+
+  const containerStyles: React.CSSProperties = {
+    display: 'flex',
+    alignItems: 'center'
+  }
+  const arrowStyles: React.CSSProperties = {
+    fontSize: 48
+  }
+
+  return (
+    <div style={containerStyles}>
+      <i
+        className="material-icons"
+        style={arrowStyles}
+        onClick={() => setCurrentIndex((currentIndex - 1) >= 0 ? currentIndex -1 : props.images.length-1 )}
+      >keyboard_arrow_left</i>
+      <FillerImage color={props.images[currentIndex]} />
+      <i
+        className="material-icons"
+        style={arrowStyles}
+        onClick={() => setCurrentIndex((currentIndex + 1) % props.images.length)}
+      >keyboard_arrow_right</i>
+    </div>
   )
 }
+
+export default ImageCarosel;
+
+
+const FillerImage: React.FC<{color: string}> = (props) => (
+  <div style={{backgroundColor: props.color, width: 200, height: 200}} />
+)
