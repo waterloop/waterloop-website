e5760529aed9a9d6c6d129ea52e8a4c8fb6387ad
georgezhyuan <yuangeorge9981@gmail.com>
2020-02-20 18:13:02 -0500

    sample typescript layout responsive to clicks

--- waterloop-site/src/App.js
+++ waterloop-site/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Test from "./pages/TestPage.tsx"
+import TeamPage from "./pages/TeamPage.tsx"
 
 function App() {
   return (
     <div className="App">
       <Test></Test>
+      <TeamPage />
     </div>
   );
 }
---
+++ waterloop-site/src/pages/TeamPage.tsx
@@ -0,0 +1,96 @@
+import React from 'react'
+
+const testData = {
+  name: "name",
+  position: "position",
+  blurb: "blurb",
+  image: "image",
+  contact: {
+    link: "link",
+    icon: "icon"
+  }
+}
+
+type Profile = {
+  name: string,
+  position: string,
+  blurb: string,
+  image: string,
+  contact: {
+    link: string,
+    icon: string
+  }
+}
+
+// Sample Expanded (Big) Profile component
+const ExpandedProfile = (props: any) => {
+  return (
+    <span style={{display: "flex", border: "thick solid #000000"}}>
+      <p>This is an Expanded profile</p>
+    </span>
+  )
+}
+
+// Sample Mini Profile component
+const MiniProfile = (props: any) => {
+  return (
+    <span
+      onClick={props.onClick}
+    >
+      <p>{props.name}</p>
+    </span>
+  )
+}
+
+interface PSectionProps {
+  profiles: Array<Profile>
+}
+
+interface PSectionStates {
+  expanded: Profile,
+  minified: Array<Profile>
+}
+
+// Profile Subsection
+class ProfileSection extends React.Component<PSectionProps, PSectionStates> {
+  constructor(props: PSectionProps) {
+    super(props)
+    this.state = {
+      expanded: null,
+      minified: this.props.profiles
+    }
+  }
+
+  ExpandProfile (i: number) {
+    alert(`PROFILE ${i} was clicked`)
+  }
+
+  render () {
+    return (
+      <div style={{border: "thick solid #000000"}}>
+        {this.state.minified.map((profile, i) => {
+          const { name, position, image } = profile
+          return <MiniProfile
+            name={name}
+            position={position}
+            image={image}
+            onClick={() => this.ExpandProfile(i)}
+          />
+        })}
+      </div>
+    )
+  }
+}
+
+// TeamPage
+export default class TeamPage extends React.Component {
+  render () {
+    return (
+      <>
+        <ProfileSection profiles={[testData, testData, testData]} />
+        <ProfileSection profiles={[testData, testData, testData]} />
+        <ProfileSection profiles={[testData, testData, testData]} />
+      </>
+    )
+  }
+}
