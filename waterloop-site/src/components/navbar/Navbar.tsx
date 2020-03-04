c06dd6d46ae261c7d0503f9fec2d39c5d9dafdd8
YESHU JAIN <y5jain@edu.uwaterloo.ca>
2020-03-03 23:58:13 -0500

    added links

--- waterloop-site/src/components/navbar/Navbar.tsx
+++ waterloop-site/src/components/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import TestComponent from "../../pages/TestPage";
 // import Home from "../home/home.tsx";
 // import GooseV from "../gooseV/gooseV.tsx";
@@ -6,69 +7,107 @@ import TestComponent from "../../pages/TestPage";
 // import Sponsors from "../sponsors/sponsors.tsx";
 // import Media from "../media/media.tsx";
 // import Contact from "../contact/contact.tsx";
+import IconYellowImg from "../../Icon_Yellow.png";
 
-import { Route, Switch, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 type Page = { title: string };
+
+const NavbarContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 5vh;
+  background-color: #232635;
+`;
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: left;
+  flex-wrap: nowrap;
+`;
+
+const LinkContainer = styled.div`
+  color: white;
+  margin: 0 5pt 0 5pt;
+`;
+
+const LogoContainer = styled.div`
+  align-items: left;
+`;
+
+const IconYellow = styled.img`
+  width: 50px;
+  height: 50px;
+  -webkit-transition: 0.2s ease-in-out;
+  transition: 0.2s ease-in-out;
+`;
+
+
 
 class Navbar extends React.Component {
   render() {
     return (
-      <div className="navbar">
-        <div className="logo">
-          <img src="../../../public/Icon_Yellow.png" />
-        </div>
-        <div className="pages">
-          <NavLink to={"/"} title="home" exact activeClassName="active">
-            HOME
-          </NavLink>
+      <NavbarContainer>
+        <LogoContainer>
+          <IconYellow src={IconYellowImg}></IconYellow>
+        </LogoContainer>
+        <ListContainer>
+          <LinkContainer>
+            <NavLink to={"/"} title="home" exact activeClassName="active">
+              HOME
+            </NavLink>
+          </LinkContainer>
 
-          <NavLink to={"/gooseV"} title="gooseV" exact activeClassName="active">
-            GOOSE V
-          </NavLink>
+          <LinkContainer>
+            <NavLink
+              to={"/gooseV"}
+              title="gooseV"
+              exact
+              activeClassName="active"
+            >
+              GOOSE V
+            </NavLink>
+          </LinkContainer>
 
-          <NavLink to={"/teams"} title="teams" exact activeClassName="active">
-            TEAMS
-          </NavLink>
+          <LinkContainer>
+            <NavLink to={"/teams"} title="teams" exact activeClassName="active">
+              TEAMS
+            </NavLink>
+          </LinkContainer>
 
-          <NavLink
-            to={"/sponsors"}
-            title="sponsors"
-            exact
-            activeClassName="active"
-          >
-            SPONSORS
-          </NavLink>
+          <LinkContainer>
+            <NavLink
+              to={"/sponsors"}
+              title="sponsors"
+              exact
+              activeClassName="active"
+            >
+              SPONSORS
+            </NavLink>
+          </LinkContainer>
 
-          <NavLink to={"/media"} title="media" exact activeClassName="active">
-            MEDIA
-          </NavLink>
+          <LinkContainer>
+            <NavLink to={"/media"} title="media" exact activeClassName="active">
+              MEDIA
+            </NavLink>
+          </LinkContainer>
 
-          <NavLink
-            to={"/contact"}
-            title="contact"
-            exact
-            activeClassName="active"
-          >
-            CONTACT
-          </NavLink>
-
-          {/* <Switch> */}
-            {/* <Route path="/" component={Home} />
-              <Route path="/gooseV" component={GooseV} />
-              <Route path="/teams" component={Teams} />
-              <Route path="/sponsors" component={Sponsors} />
-              <Route path="/media" component={Media} />
-              <Route path="/contact" component={Contact} /> */}
-            <Route path="/" component={TestComponent} />
-            <Route path="/gooseV" component={TestComponent} />
-            <Route path="/teams" component={TestComponent} />
-            <Route path="/sponsors" component={TestComponent} />
-            <Route path="/media" component={TestComponent} />
-            <Route path="/contact" component={TestComponent} />
-          {/* </Switch> */}
-        </div>
-      </div>
+          <LinkContainer>
+            <NavLink
+              to={"/contact"}
+              title="contact"
+              exact
+              activeClassName="active"
+            >
+              CONTACT
+            </NavLink>
+          </LinkContainer>
+        </ListContainer>
+      </NavbarContainer>
     );
   }
 }
