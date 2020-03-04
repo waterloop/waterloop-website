5dfa746d357d459f96df478ca9413966c83624af
Quinn1876 <qhodges0442@gmail.com>
2020-03-03 22:20:10 -0500

    Added Image Cycleing

--- waterloop-site/src/components/past-geese-timeline/image-carosel.component.tsx
+++ waterloop-site/src/components/past-geese-timeline/image-carosel.component.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+// To be replaced by an API call. Mock data for now
+// Delete the local files when the API is created
+import Goose1 from './goose1.png'
+import Goose2 from './goose2.png'
+import GooseEx from './goosex.png'
+
 interface Props {
   images: string[]
 }
 
-const ImageCarosel: React.FC<Props> = (props) => {
+const ImageCarosel: React.FC = (props) => {
 
   const [currentIndex, setCurrentIndex] = React.useState<number>(0);
 
@@ -13,22 +19,33 @@ const ImageCarosel: React.FC<Props> = (props) => {
     alignItems: 'center'
   }
   const arrowStyles: React.CSSProperties = {
-    fontSize: 48
+    fontSize: 48,
+    cursor: 'pointer',
   }
+
+  const imgs = [Goose1, Goose2, GooseEx]
+
+  const cycleRight = () => setCurrentIndex((currentIndex + 1) % imgs.length)
+  const cycleLeft = () => setCurrentIndex((currentIndex - 1) >= 0 ? currentIndex -1 : imgs.length-1 )
 
   return (
     <div style={containerStyles}>
       <i
         className="material-icons"
         style={arrowStyles}
-        onClick={() => setCurrentIndex((currentIndex - 1) >= 0 ? currentIndex -1 : props.images.length-1 )}
-      >keyboard_arrow_left</i>
-      <FillerImage color={props.images[currentIndex]} />
+        onClick={cycleLeft}
+      >
+        keyboard_arrow_left
+      </i>
+      {/* <FillerImage color={props.images[currentIndex]} /> */}
+      <img src={imgs[currentIndex]}  alt="Goose Pick"/>
       <i
         className="material-icons"
         style={arrowStyles}
-        onClick={() => setCurrentIndex((currentIndex + 1) % props.images.length)}
-      >keyboard_arrow_right</i>
+        onClick={cycleRight}
+      >
+        keyboard_arrow_right
+      </i>
     </div>
   )
 }
