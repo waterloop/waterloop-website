7b60ccf1208c9702fc2e7c0563351ec13f5d005c
Talia <t429wang@uwaterloo.ca>
2020-03-06 16:44:02 -0500

    added responsive

--- waterloop-site/src/components/SubteamsOverview.tsx
+++ waterloop-site/src/components/SubteamsOverview.tsx
@@ -3,23 +3,39 @@ import styled from "styled-components"
 
 const TextBlock = styled.div`
   text-align: justify;
-  width: 500px;
+  width: 50%;
   height: 100%;
   padding-left: 3%;
   display: inline-block;
+  @media (max-width: 500px){
+    display: block;
+    text-align: center;
+    margin-left: 25%;
+    padding-left: 0;
+    margin-right: 25%;
+  }
 `;
 
 const Img = styled.img`
-  width: 240px;
-  height: 185px;
+  margin-top: 2%;
+  width: 25%;
+  height: 17vw;
+  @media (max-width: 500px){
+    width: 40%;
+    height: 27vw;
+  }
 `;
 
 const Header = styled.h2`
   font-style: italic;
-  font-size: 125%;
+  font-size: 1.7vw;
+  @media (max-width: 500px){
+    font-size: 2vw;
+  }
 `;
 
 const Text = styled.p`
+  font-size: 1.5vw;
 `;
 
 type MyProps = {subteam: string, description: string, image: string};
@@ -28,7 +44,7 @@ class SubteamsOverview extends React.Component <MyProps> {
 
   render() {
     return (
-      <div style={{textAlign: "center"}}>
+      <div id = "div" style={{textAlign: "center"}}>
         <Img src = {this.props.image} alt = "photo"></Img>
         <TextBlock>
           <Header>{this.props.subteam}</Header>
