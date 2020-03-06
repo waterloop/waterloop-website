97c0b52d1241348afac92de27421a98ca53c6abc
georgezhyuan <yuangeorge9981@gmail.com>
2020-03-06 18:03:54 -0500

    temp commit for branch switch

---
+++ waterloop-site/src/components/ExpandedProfile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const StyledExpanededProfile = styled.div`
+  width: 250px;
+  height: 200px;
+  background-color: gray;
+`
+
+export default ExpandedProfile = () => {
+  return <StyledExpanededProfile />
+}
