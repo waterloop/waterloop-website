0099649ea7cec4d57257b88df838fa2a0e517e45
zafin-hassan <zafin.hassan@gmail.com>
2020-03-06 04:50:03 -0500

    made sponsor component resonsive

--- waterloop-site/src/components/SponsorComponent.tsx
+++ waterloop-site/src/components/SponsorComponent.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
+const size = {
+  mobile: "420px",
+  tabletM: "800px",
+  tabletL: "1100px"
+};
+
 const SponsorComponent = styled.div`
   position: relative;
   width: 495px;
@@ -12,6 +18,11 @@ const SponsorComponent = styled.div`
   background: #f4f4f4;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 15px;
+  @media (max-width: ${size.mobile}) {
+    height: 184px;
+    width: 80%;
+    margin-bottom: 5%;
+  }
 `;
 
 export default SponsorComponent;
