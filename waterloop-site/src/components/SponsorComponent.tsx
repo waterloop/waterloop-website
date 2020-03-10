dd1da0c3af7127b1edf67b6749666c196f3dce1d
zafin-hassan <zafin.hassan@gmail.com>
2020-03-09 20:00:55 -0400

    refactored sponsor component with styled-system

--- waterloop-site/src/components/SponsorComponent.tsx
+++ waterloop-site/src/components/SponsorComponent.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { variant } from "styled-system";
 
 const size = {
   mobile: "420px",
@@ -7,22 +8,21 @@ const size = {
   tabletL: "1100px"
 };
 
-const SponsorComponent = styled.div`
-  position: relative;
-  width: 80%;
-  height: 250px;
-  margin-bottom: 10px;
-  display: flex;
-  align-items: center;
-
-  background: #f4f4f4;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 15px;
-  @media (max-width: ${size.mobile}) {
-    height: 184px;
-    width: 80%;
-    margin-bottom: 5%;
+const SponsorComponent = styled("div")({
+  position: "relative",
+  width: "80%",
+  height: "250px",
+  marginBottom: "10px",
+  display: "flex",
+  alignItems: "center",
+  background: "#f4f4f4",
+  boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+  borderRadius: "15px",
+  "@media (max-width: ${size.mobile})": {
+    height: "184px",
+    width: "80%",
+    marginNottom: "5%"
   }
-`;
+});
 
 export default SponsorComponent;
