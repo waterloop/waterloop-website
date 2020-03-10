807136725789220ab9ea612301565398789616d2
georgezhyuan <yuangeorge9981@gmail.com>
2020-03-10 17:21:30 -0400

    styled the team lead title

--- waterloop-site/src/pages/TeamPage.tsx
+++ waterloop-site/src/pages/TeamPage.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
+
 import ProfileSection from '../components/ProfileSection'
 
 import testData from '../testProfileData'
+
+const ProfileSectionTitle = styled.h1`
+  font-family: IBM Plex Sans;
+  font-style: italic;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 47px;
+  color: #010101;
+  margin-bottom: 50px;
+`
 
 export default class TeamPage extends React.Component {
   render () {
     console.log(testData)
     return (
       <div>
-        <h1>Leads 1</h1>
+        <ProfileSectionTitle>Leads 1</ProfileSectionTitle>
         <ProfileSection profiles={testData.slice(0,3)} />
 
-        <h1>Leads 2</h1>
+        <ProfileSectionTitle>Leads 2</ProfileSectionTitle>
         <ProfileSection profiles={testData.slice(3,9)} />
       </div>
     )
