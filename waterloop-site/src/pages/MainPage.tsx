4cca61fec458621bb5df41adf16522da3d486c80
zafin-hassan <zafin.hassan@gmail.com>
2020-03-18 14:11:34 -0400

    sample code with content wrapper

--- waterloop-site/src/pages/MainPage.tsx
+++ waterloop-site/src/pages/MainPage.tsx
@@ -2,31 +2,51 @@ import React from "react";
 import styled from "styled-components";
 import FooterComponent from "../components/FooterComponent";
 import { BrowserRouter as Router } from "react-router-dom";
-import PositionPosting from "../components/recruitment_form_component/PositionPosting"
+import PositionPosting from "../components/recruitment_form_component/PositionPosting";
+
+const ContentWrapper = styled.div`
+  position: relative;
+`;
 
 class MainPage extends React.Component {
   render() {
     return (
       <div>
+        <ContentWrapper>
+          <PositionPosting
+            role="Goose Designer"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            skills="JavaScript, Goose Handling, Pain Tolerance"
+          ></PositionPosting>
+          <PositionPosting
+            role="Goose Designer"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            skills="JavaScript, Goose Handling, Pain Tolerance"
+          ></PositionPosting>
+          <PositionPosting
+            role="Goose Designer"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            skills="JavaScript, Goose Handling, Pain Tolerance"
+          ></PositionPosting>
+          <PositionPosting
+            role="Goose Designer"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            skills="JavaScript, Goose Handling, Pain Tolerance"
+          ></PositionPosting>
+          <PositionPosting
+            role="Goose Designer"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            skills="JavaScript, Goose Handling, Pain Tolerance"
+          ></PositionPosting>
+          <PositionPosting
+            role="Goose Designer"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            skills="JavaScript, Goose Handling, Pain Tolerance"
+          ></PositionPosting>
+        </ContentWrapper>
         <Router>
           <FooterComponent />;
         </Router>
-
-        <PositionPosting
-          role ="Goose Designer"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          skills = "JavaScript, Goose Handling, Pain Tolerance"
-        ></PositionPosting>
-        <PositionPosting
-          role ="Goose Designer"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          skills = "JavaScript, Goose Handling, Pain Tolerance"
-        ></PositionPosting>
-        <PositionPosting
-          role ="Goose Designer"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          skills = "JavaScript, Goose Handling, Pain Tolerance"
-        ></PositionPosting>
       </div>
     );
   }
