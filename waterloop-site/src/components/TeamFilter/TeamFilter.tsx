4d3d204d6fbd399493d42ee5eb225c954cb61394
Catherine Lee <Catherine@Catherines-MBP.phub.net.cable.rogers.com>
2020-03-26 02:43:46 -0400

    Added comments

--- waterloop-site/src/components/TeamFilter/TeamFilter.tsx
+++ waterloop-site/src/components/TeamFilter/TeamFilter.tsx
@@ -26,10 +26,12 @@ const StyledFilter = styled.button`
     background-color: #222535;
   }
 
+  //web buttons disappear when the width is less than 975px
   @media (max-width: 975px){
     display: none;
   }
 
+  // mobile buttons
   &.mobile {
     text-align: left;
     padding-left: 30px;
@@ -39,15 +41,18 @@ const StyledFilter = styled.button`
     width: 300px;
     height: 70px;
 
+    //displays when width is less than 975px
     @media (max-width: 975px){
       display: unset;
     }
 
+    //diasppears when width is more than 975px
     @media (min-width: 975px){
       display: none;
     }
   }
 
+  //select team button when mobile toggle is closed
   &.selectTeams {
     width: 375px;
     height: 70px;
@@ -58,21 +63,25 @@ const StyledFilter = styled.button`
     text-align: left;
     padding-left: 30px;
 
+    //displays when width is less than 975px
     @media (max-width: 975px){
       display: unset;
     }
 
+    //diasppears when width is more than 975px
     @media (min-width: 975px){
       display: none;
     }
   }
 `
 
+//button colours when selected
 const SelectedFilter = styled(StyledFilter)`
   color: white;
   background-color: #222535;
 `
 
+//container for buttons in mobile
 const SlideDownContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -83,6 +92,8 @@ const SlideDownContainer = styled.div`
     display: none;
   }
 `
+
+//mobile toggle button 
 const StyledToggle = styled.button`
   display: flex;
   flex-direction: column;
@@ -99,12 +110,14 @@ const StyledToggle = styled.button`
     display: none;
   }
 `
+//lines of the mobile toggle button
 const ToggleLine = styled.div`
   width: 55px;
   height: 8px;
   background-color: #F9D55B;
   border-radius: 10px;
 
+  //toggle line color when toggle button is selected
   &.open{
     background-color: #808080;
   }
@@ -114,8 +127,11 @@ const TeamFilter = (props: any) => {
   let slideDownContainer;
   let toggleLine;
 
+  //toggle is open: slide down container contains the team filter buttons in mobile format and toggle lines become grey
+  //toggle is closed: slide down container contains the select team button and toggle lines are yellow
   if (props.filters.toggleOpen) {
     slideDownContainer = <SlideDownContainer >{props.filters.teamFilters.map((filter: any, i: number) => {
+       //checks if filter has been applied and changes style accordingly
       const Button = filter ? SelectedFilter : StyledFilter;
       return <Button className="mobile" key={i} onClick={() => props.updateFilters(i)}>Some Filter</Button>
       })}</SlideDownContainer>
@@ -129,9 +145,10 @@ const TeamFilter = (props: any) => {
   return (
     <FitlerContainer>
       {props.filters.teamFilters.map((filter: any, i: number) => {
-    const Button = filter ? SelectedFilter : StyledFilter;
-    return <Button key={i} onClick={() => props.updateFilters(i)}>Some Filter</Button>
-    })}
+       //checks if filter has been applied and changes style accordingly
+      const Button = filter ? SelectedFilter : StyledFilter;
+      return <Button key={i} onClick={() => props.updateFilters(i)}>Some Filter</Button>
+      })}
       {slideDownContainer}
       <StyledToggle onClick={() => props.updateToggle()}>
         {toggleLine}
