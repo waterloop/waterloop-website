a3f8b972f725fd2b8ed9dbe2738ce7e2f404967c
jxrin <jerin.raisa11@gmail.com>
2020-04-29 16:18:24 -0400

    changing file structure for contact form component

--- waterloop-site/src/pages/MainPage.tsx
+++ waterloop-site/src/pages/MainPage.tsx
@@ -3,15 +3,22 @@ import FooterComponent from "../sections/Footer/index";
 import { BrowserRouter as Router } from "react-router-dom";
 import Sponsors from "../components/Sponsors/index";
 import Subteams from "../components/Teams";
+import ContactForm from "../components/ContactForm/index";
 import NavBar from "../components/NavBar/index";
 import Sample from "../static/img/logos/Icon_Yellow.png";
 
 class MainPage extends React.Component {
   render() {
     return (
-      <Router>
-        <NavBar></NavBar>
-      </Router>
+      <div>
+        <Router>
+          <NavBar></NavBar>
+        </Router>
+        <ContactForm
+          desc="Desc"
+          title="Send Us A Message"
+        ></ContactForm>
+      </div>
     );
   }
 }
