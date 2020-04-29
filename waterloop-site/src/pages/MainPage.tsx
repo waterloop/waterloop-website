69ed44f07c03f49ecb3409bf6237cd3ea60c921f
jxrin <jerin.raisa11@gmail.com>
2020-04-29 15:41:20 -0400

    :zap: update file structure :zap:

--- waterloop-site/src/pages/MainPage.tsx
+++ waterloop-site/src/pages/MainPage.tsx
@@ -3,29 +3,13 @@ import FooterComponent from "../sections/Footer/index";
 import { BrowserRouter as Router } from "react-router-dom";
 import Sponsors from "../components/Sponsors/index";
 import Subteams from "../components/Teams";
-import Button from "../components/button/CTAButton";
+import Button from "../components/Button/CTAButton";
 import Sample from "../static/img/logos/Icon_Yellow.png";
 
 class MainPage extends React.Component {
   render() {
     return (
       <Router>
-        <Subteams
-          image={Sample}
-          description="gjfdkbfdaf fndjsafjnkd fdsjakfndjksanjk nfjkdsnakf ndskanfjkdsanfjkdsanjfkdsankjfn"
-          subteam="Electrical"
-        ></Subteams>
-        <Button
-          backgroundColor="yellow"
-          textColor="black"
-          text="Send"
-          onClick={() => {
-            document.getElementById("submitBtn")?.click();
-          }}
-          variant={null}
-        ></Button>
-        <Sponsors></Sponsors>
-        <FooterComponent />;
       </Router>
     );
   }
