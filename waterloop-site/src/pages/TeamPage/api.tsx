3892de7d42feb8f54c493d9e8ba773475c0e68d8
georgezhyuan <yuangeorge9981@gmail.com>
2020-05-18 21:18:14 -0400

    updated bearer token for teamhub api

--- waterloop-site/src/pages/TeamPage/api.tsx
+++ waterloop-site/src/pages/TeamPage/api.tsx
@@ -2,7 +2,7 @@
 const teamHubApiEndpoint = `https://cors-anywhere.herokuapp.com/https://hub.waterloop.ca/api`
 const teamHubHeader = {
   authorization:
-    "Bearer 16be4713087d2dab1a481b4e76de87ad9945cd31c385036178dee49adbe244648ee715fb60cdf7fdd4789bd1182f0252884e4d4957adeaed0d02ea37a735a8f1"
+    "Bearer d9c6bf2221a7c4475bab79e058701ae18bfecd4b33f1af75c42bc47dade26f580fde63bbea07b774a176d4252872dc32959b45ddf84fa2e7625acb0d1608c12e"
 }
 
 // Generate query params for fetch requests
