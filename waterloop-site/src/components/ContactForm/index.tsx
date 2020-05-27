4a1c57fa6482b2096b465f1d75758407030ee193
Stephen Lin <linsteph@Stephens-MacBook-Pro-2.local>
2020-05-27 17:49:30 -0400

    Added barrel files

--- waterloop-site/src/components/ContactForm/index.tsx
+++ waterloop-site/src/components/ContactForm/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import Button from "../Button/CTAButton";
+import {Button} from "../button";
 
 type ContactFormProps = {
   title: string;
