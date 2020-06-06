1fde1a1a2d331f32d6addf1586f3e50b68ca45dc
georgezhyuan <yuangeorge9981@gmail.com>
2020-06-06 00:13:08 -0400

    dynamically insert postings

--- waterloop-site/src/pages/Recruitment.tsx
+++ waterloop-site/src/pages/Recruitment.tsx
@@ -2,43 +2,51 @@ import React from 'react'
 import PositionPosting from "../components/recruitment_form_component/PositionPosting";
 import Form from "../components/recruitment_form_component/Form";
 
-const samplePostingsData = {
-
-}
+const samplePostingsData = [
+    {
+      role : "Goose Designer",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      skills: "JavaScript, Goose Handling, Creativity",
+      technicalQ : "How would you design a nest?",
+      termList: ["F19", "W20", "S20", "F20", "W21"]
+    },
+    {
+      role: "Goose Destroyer",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      skills: "JavaScript, Goose Handling, Resilience",
+      technicalQ: "What is the most deadly feature of a goose?",
+      termList: ["F19", "W20", "S20", "F20", "W21"],
+    },
+    {
+      role: "Goose Whisperer",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      skills: "JavaScript, Goose Handling, Patience",
+      technicalQ: "Demonstrate a time you translated geese language",
+      termList: ["F19", "W20", "S20", "F20", "W21"]
+    },
+    {
+      role: "Goose Babysitter",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      skills: "JavaScript, Goose Handling, Kindness",
+      technicalQ: "How would you care for the geese in Waterloo?",
+      termList: ["F19", "W20", "S20", "F20", "W21"]
+    }
+]
 
 class Recruitment extends React.Component<any,any> {
   render() {
     return (
-      <div>
-        <PositionPosting
-          role ="Goose Designer"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          skills = "JavaScript, Goose Handling, Creativity"
-          technicalQ = "How would you design a nest?"
-          termList = {["F19", "W20", "S20", "F20", "W21"]}
-        ></PositionPosting>
-        <PositionPosting
-          role ="Goose Destroyer"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          skills = "JavaScript, Goose Handling, Resilience"
-          technicalQ = "What is the most deadly feature of a goose?"
-          termList = {["F19", "W20", "S20", "F20", "W21"]}
-        ></PositionPosting>
-        <PositionPosting
-          role ="Goose Whisperer"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          skills = "JavaScript, Goose Handling, Patience"
-          technicalQ = "Demonstrate a time you translated geese language"
-          termList = {["F19", "W20", "S20", "F20", "W21"]}
-        ></PositionPosting>
-        <PositionPosting
-          role ="Goose Babysitter"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          skills = "JavaScript, Goose Handling, Kindness"
-          technicalQ = "How would you care for the geese in Waterloo?"
-          termList = {["F19", "W20", "S20", "F20", "W21"]}
-        ></PositionPosting>
-      </div>
+      <>
+        { samplePostingsData.length > 0 && samplePostingsData.map((item: any) => {
+          return <PositionPosting
+            role = {item.role}
+            description = {item.description}
+            skills = {item.skills}
+            technicalQ = {item.technicalQ}
+            termList = {item.termList}
+          ></PositionPosting>
+        })}
+      </>
     )
   }
 }
