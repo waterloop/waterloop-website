fbb8f00edeed03bea08cd87145f8737e27256394
georgezhyuan <yuangeorge9981@gmail.com>
2020-06-05 23:57:15 -0400

    put recruitment page into separate file

--- waterloop-site/src/pages/MainPage.tsx
+++ waterloop-site/src/pages/MainPage.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import FooterComponent from "../components/FooterComponent";
+import Recruitment from "./Recruitment";
 import { BrowserRouter as Router } from "react-router-dom";
-import PositionPosting from "../components/recruitment_form_component/PositionPosting";
-import Form from "../components/recruitment_form_component/Form";
 
 const ContentWrapper = styled.div`
   position: relative;
@@ -13,37 +12,8 @@ class MainPage extends React.Component {
   render() {
     return (
       <div>
-
-        <PositionPosting
-          role ="Goose Designer"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          skills = "JavaScript, Goose Handling, Creativity"
-          technicalQ = "How would you design a nest?"
-          termList = {["F19", "W20", "S20", "F20", "W21"]}
-        ></PositionPosting>
-        <PositionPosting
-          role ="Goose Destroyer"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          skills = "JavaScript, Goose Handling, Resilience"
-          technicalQ = "What is the most deadly feature of a goose?"
-          termList = {["F19", "W20", "S20", "F20", "W21"]}
-        ></PositionPosting>
-        <PositionPosting
-          role ="Goose Whisperer"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          skills = "JavaScript, Goose Handling, Patience"
-          technicalQ = "Demonstrate a time you translated geese language"
-          termList = {["F19", "W20", "S20", "F20", "W21"]}
-        ></PositionPosting>
-        <PositionPosting
-          role ="Goose Babysitter"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          skills = "JavaScript, Goose Handling, Kindness"
-          technicalQ = "How would you care for the geese in Waterloo?"
-          termList = {["F19", "W20", "S20", "F20", "W21"]}
-        ></PositionPosting>
-
         <Router>
+          <Recruitment />;
           <FooterComponent />;
         </Router>
       </div>
