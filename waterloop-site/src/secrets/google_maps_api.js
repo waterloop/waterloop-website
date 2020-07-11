e075ec544a778466457486ab41ec7a1cb5a0f966
jxrin <jerin.raisa11@gmail.com>
2020-07-11 01:37:09 -0400

    removing file

--- waterloop-site/src/secrets/google_maps_api.js
+++
@@ -1,4 +0,0 @@
-const GoogleMapsAPI =
-  '<iframe src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ_8ztAq32K4gRF44mljnynd0&key=AIzaSyCGLLjvTMb0xjl-I3-Dyqp9aJGSIvvh4GA"></iframe>';
-
-export default GoogleMapsAPI;
