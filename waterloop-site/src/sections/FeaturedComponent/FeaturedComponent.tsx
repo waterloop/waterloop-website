7108dce6704fe1ae36569fa814066bffe23bb4c2
Lynn <lynnkim2000@gmail.com>
2020-07-10 22:29:00 -0400

    change import links

--- waterloop-site/src/sections/FeaturedComponent/FeaturedComponent.tsx
+++ waterloop-site/src/sections/FeaturedComponent/FeaturedComponent.tsx
@@ -5,28 +5,32 @@ import * as data from "../../static/copy/exFeatsModel.json";
 import "../../theme/Global.css";
 
 class FeatPage extends React.Component {
-    // Use this to assemble the feature sections with proper XML tags for the render area.
-    assembleFeatSections(featInfo: Array<any>) {
-        var features: JSX.Element[] = [];
-        let assetDir = "static/img/assets/"; // NOTE: I dunno how to properly reference the assets directory so this is the workaround + require statements.
+  // Use this to assemble the feature sections with proper XML tags for the render area.
+  assembleFeatSections(featInfo: Array<any>) {
+    var features: JSX.Element[] = [];
+    let assetDir = "static/img/goose/"; // NOTE: I dunno how to properly reference the assets directory so this is the workaround + require statements.
 
-        featInfo.forEach((value) => {
-            features.push(
-                // Define styled component here:
-                <SimpleFeatComp imgSrc={assetDir + value.imgSrc} imgAlt={value.imgAlt} desc={value.desc}></SimpleFeatComp>
-            )
-        });
+    featInfo.forEach((value) => {
+      features.push(
+        // Define styled component here:
+        <SimpleFeatComp
+          imgSrc={assetDir + value.imgSrc}
+          imgAlt={value.imgAlt}
+          desc={value.desc}
+        ></SimpleFeatComp>
+      );
+    });
 
-        return features;
-    }
-    // Render feature data passed from the model section 
-    render() {
-        return (
-            <div>
-                <h2 className="Header-FeaturedComponent">Features</h2>
-                {this.assembleFeatSections(data.features)}
-            </div>
-        )
-    }
+    return features;
+  }
+  // Render feature data passed from the model section
+  render() {
+    return (
+      <div>
+        <h2 className="Header-FeaturedComponent">Features</h2>
+        {this.assembleFeatSections(data.features)}
+      </div>
+    );
+  }
 }
 export default FeatPage;
