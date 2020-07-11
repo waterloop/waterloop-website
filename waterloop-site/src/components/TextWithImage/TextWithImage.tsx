450ab9b65ca289018b5f3d90a8e989527b567a16
Lynn <lynnkim2000@gmail.com>
2020-07-10 22:46:59 -0400

    global css stuff

--- waterloop-site/src/components/TextWithImage/TextWithImage.tsx
+++ waterloop-site/src/components/TextWithImage/TextWithImage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "components/Button/";
+import { Button } from "components/Button";
 import "../../theme/global.css";
 
 type MyProps = { title: string; text: string; image: string };
