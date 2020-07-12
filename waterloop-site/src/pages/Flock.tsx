65d784a9f0ade342429de6eefbcd79c008f2e423
jxrin <jerin.raisa11@gmail.com>
2020-07-12 01:47:15 -0400

    removed components on flock page

--- waterloop-site/src/pages/Flock.tsx
+++ waterloop-site/src/pages/Flock.tsx
@@ -1,23 +1,11 @@
 import React from "react";
 import Hero from "components/Hero/General"
-import SimpleFeature from "components/SimpleFeatures/SimpleFeature"
-import TextWithImageData from "../static/copy/TextWithImage.json"
-import PastGeeseTimeline from "components/PastGeeseTimeline/PastGeeseTimeline"
-
-var image = "";
-
-TextWithImageData.forEach((detail) => {
-    image = detail.image;
-})
 
 class Flock extends React.Component {
     render() {
         return (
             <div>
                 <Hero title="The Flock" description="Learn about our geese."></Hero>
-                <SimpleFeature imgSrc={image} imgAlt="test" desc="test"></SimpleFeature>
-                <SimpleFeature imgSrc={image} imgAlt="test" desc="test"></SimpleFeature>
-                <PastGeeseTimeline className=""></PastGeeseTimeline>
             </div>
         );
     }
