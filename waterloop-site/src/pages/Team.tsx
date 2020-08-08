98054d3973c48685440fddba175e6154a4d3db59
Jeff Ma <jeff-2c@hotmail.com>
2020-08-08 19:38:45 -0400

    Remove anchor tags as they're no longer needed.

--- waterloop-site/src/pages/Team.tsx
+++ waterloop-site/src/pages/Team.tsx
@@ -9,29 +9,10 @@ const ContentContainer = styled.div`
   max-width: 1080px;
   margin: 0 auto;
 `
-const JumpPoint = styled.div`
-`
-const MiniNav = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin-top: 30px;
-`
-const MiniNavItem = styled.span`
-  margin-right: 20px;
-  font-weight: bold;
-`
 
 class Teams extends React.Component<any, any> {
   constructor(props: any) {
     super(props)
-    this.state = {
-      intro: React.createRef(),
-      roster: React.createRef()
-    }
-  }
-
-  scrollToRef(ref: any) {
-    window.scrollTo(0, ref.current.offsetTop)
   }
 
   render() {
@@ -40,16 +21,8 @@ class Teams extends React.Component<any, any> {
         <Hero title="Meet The Team" description="Learn about our geese." />
 
         <ContentContainer>
-          <MiniNav>
-            <MiniNavItem onClick={() => this.scrollToRef(this.state.intro)}><h6>Our Team</h6></MiniNavItem>
-            <MiniNavItem><h6> \\ </h6></MiniNavItem>
-            <MiniNavItem onClick={() => this.scrollToRef(this.state.roster)}><h6>Team Roster</h6></MiniNavItem>
-          </MiniNav>
-
-          <JumpPoint ref={this.state.intro} />
           <OurTeam/>
 
-          <JumpPoint ref={this.state.roster} />
           <TeamsDisplayer initFilterSetting={0} />
         </ContentContainer>
       </div>
