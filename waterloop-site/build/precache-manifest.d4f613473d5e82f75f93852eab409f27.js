48013450acf37f5c2f2c840a7e9e956d6e9cc710
jxrin <jerin.raisa11@gmail.com>
2020-08-13 00:54:57 -0400

    removing comments

---
+++ waterloop-site/build/precache-manifest.d4f613473d5e82f75f93852eab409f27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e29c81804c7b2a2d8a068184915901",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77e6bce4d3a481477d83",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "640ff55e0b77845dc241",
+    "url": "/static/js/2.bbe61cca.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.bbe61cca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77e6bce4d3a481477d83",
+    "url": "/static/js/main.442c7ca1.chunk.js"
+  },
+  {
+    "revision": "4d91a6bf095055f3c174",
+    "url": "/static/js/runtime-main.b7242014.js"
+  }
+]);
