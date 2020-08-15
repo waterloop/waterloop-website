a0158e545e8f4e3c0dc76fc24585ee5637162d3a
Lynn <lynnkim2000@gmail.com>
2020-08-15 16:16:58 -0400

    merge

---
+++ waterloop-site/src/components/RecruitmentForm/RadioButton.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface RadioButtonProps {
+  checked: boolean,
+  onChange: () => void,
+  name: string,
+}
+
+const RadioContainer = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  margin-right: 8px;
+`;
+
+const RadioLabel = styled.label`
+`;
+
+const RadioButton: React.FC<RadioButtonProps> = ({ checked, onChange, name }) => (
+  <RadioContainer>
+    <RadioLabel htmlFor={`radiobutton-${name}`} onClick={onChange}>
+      {name}
+    </RadioLabel>
+    <input
+      id={`radiobutton-${name}`}
+      type="radio"
+      checked={checked}
+      name="applicationTerm"
+      required
+      onChange={onChange}
+    />
+  </RadioContainer>
+);
+
+export default RadioButton;
