ced19a5cb10e8b2e3a32acd381e367438b8543b9
jxrin <jerin.raisa11@gmail.com>
2020-08-23 19:18:55 -0400

    fixing mobile bug

--- waterloop-site/src/components/TeamsDisplayer/components/Profiles/SubProfile.tsx
+++ waterloop-site/src/components/TeamsDisplayer/components/Profiles/SubProfile.tsx
@@ -24,8 +24,8 @@ const PortraitContainer = styled.div`
   }
 `;
 const Portrait = styled.img`
-  height: 104px;
-  width: 104px;
+  height: 105px;
+  width: 105px;
   border-radius: 50% 50%;
   border: 4px solid #fbc531;
 `;
