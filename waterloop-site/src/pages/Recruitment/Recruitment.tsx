b051bd23a5b5bb4ce7b6e8ee75c957b721053dc7
Quinn1876 <qhodges0442@gmail.com>
2021-04-13 22:40:13 -0400

    Added form signup

--- waterloop-site/src/pages/Recruitment/Recruitment.tsx
+++ waterloop-site/src/pages/Recruitment/Recruitment.tsx
@@ -8,6 +8,7 @@ import JoinUs from "../../static/img/team/joinus.png";
 import "../../theme/styles.scss";
 
 import JobPostings from "components/Postings/Postings";
+import { Button } from "components";
 const castData: MyProps[] = Text;
 const imgData: string[] = [JoinUs];
 
@@ -16,6 +17,10 @@ const currentlyRecruiting = false;
 const FlexContainer = styled.div`
   display: flex;
   flex-direction: column;
+`;
+
+const SignUpButton = styled(Button)`
+  width: 128px;
 `;
 
 
@@ -43,8 +48,9 @@ const Recruitment: React.FC = () => (
         <FlexContainer>
           <h3>Our new member recruitment is now <b>closed</b> for the Winter term.</h3>
           <p>
-          Our new member recruitment for the Spring term is not open yet. Thanks for visiting and be sure to check back in the first week of May to join the team! :) Do you have prior expertise in linear induction motors, high power PCB design, or other relevant technical topics? We'd love to learn from you as an advisor; reach out to us at <a href = "mailto: contact@waterloop.ca">contact@waterloop.ca</a> if you're interested!
+          Our new member recruitment for the Spring term will be opening soon. Click below to sign up for our newsletter and be notified when postings go live. Thanks for visiting and be sure to check back in the first week of May to join the team! :) Do you have prior expertise in linear induction motors, high power PCB design, or other relevant technical topics? We'd love to learn from you as an advisor; reach out to us at <a href = "mailto: contact@waterloop.ca">contact@waterloop.ca</a> if you're interested!
           </p>
+          <SignUpButton onClick={() => window.open('http://wloop.ca/subscribe')} text="Sign up" backgroundColor="yellow" textColor="black"  />
         </FlexContainer>
       )}
     </div>
