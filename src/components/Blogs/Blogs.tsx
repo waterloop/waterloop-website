f2b7c4b756ac3a50eeb1675cc1e7ed0a367f5138
Aayush Wadhwa <aayushwadhwa31@gmail.com>
2021-06-08 10:26:22 +0530

    Blog Page Design Complete

--- src/components/Blogs/Blogs.tsx
+++ src/components/Blogs/Blogs.tsx
@@ -1,51 +1,91 @@
 import BlogPost from "components/BlogLanding/BlogList"
+import { Button } from "components/Button"
 import React from "react"
 import styled from "styled-components"
 
 
+const BlogPage = styled.div`
+`
+
+const RecentPostsWrapper = styled.div`
+    margin-bottom: 40px;
+`
+
 const RecentPostDiv = styled.div`
-    padding: 20px;
+    padding:40px;
     width: 100%;
     margin: 0 auto;
+    display: inline-block;
 `
 
 
 const RecentImageDiv = styled.div`
     width: 50%;
     height: 300px;
-    padding: 20px;
     float: left;
     > .img {
         width: 100%;
         height: 100%;
         border-radius: 10px;
+        box-shadow: 0px 0px 8px 0px #ada8a8;
     }
 `
 
 const RecentPostText = styled.div`
     padding: 20px;
-    float: left;
+    float: right;
+    width: 40%;
     > .PostTitle-Blog {
         font-size: 18px;
         font-weight: bold;
         font-style: italic;
         margin-bottom: 5px;
       }
+    > .PostAuthor-Blog{
+        font-size: 12px;
+        font-weight: bold;
+        margin: 20px 0;
+    }
+    > .PostDescription-Blog {
+        margin: 10px 0;
+        font-size: 14px;
+    }
+    > .PostReadButton-Blog {
+        margin: 25px 0;
+        > .ButtonDiv {
+            padding: 10px 40px;
+            > .ButtonText {
+                font-size: 18px;
+            }
+        }
+    }
 `
 
 const Blogs: React.FC = () => (
-    <div className="Block-BlogPage">
-        <RecentPostDiv>
-            {/* <div className="Block-RecentImage">
+    <BlogPage>
 
-            </div> */}
-            <RecentImageDiv>
-                <img className="img"  src="https://picsum.photos/200/300" />
-            </RecentImageDiv>
-            <RecentPostText>
-                <div className="PostTitle-Blog">The Competition</div>
-            </RecentPostText>
-        </RecentPostDiv>
+        <RecentPostsWrapper>
+            {
+                [0, 1].map(() => (
+                    <RecentPostDiv>
+                        <RecentImageDiv>
+                            <img className="img" src="https://picsum.photos/200/300" />
+                        </RecentImageDiv>
+                        <RecentPostText>
+                            <div className="PostTitle-Blog">The Competition</div>
+                            <div className="PostAuthor-Blog">Written by: John Doe</div>
+                            <div className="PostDescription-Blog">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas accusantium dolorem, beatae possimus nesciunt ab? Illum atque doloremque fugit ipsam quibusdam eveniet magnam quod, amet id obcaecati et odio praesentium.</div>
+                            <div className="PostReadButton-Blog"><Button
+                                backgroundColor="yellow"
+                                textColor="black"
+                                text="READ"
+                                onClick={(): Window | null => window.open('blog', '_self')}
+                            /></div>
+                        </RecentPostText>
+                    </RecentPostDiv>
+                ))}
+        </RecentPostsWrapper>
+
         <div className="Block-BlogPosts">
             <div className="PostsBlock-Blog">
                 {
@@ -55,7 +95,7 @@ const Blogs: React.FC = () => (
                 }
             </div>
         </div>
-    </div>
+    </BlogPage>
 )
 
 export default Blogs
