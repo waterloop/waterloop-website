25dd43695bd9841ce382095b43181b7847f510b9
Boya <boyazang@hotmail.com>
2021-09-23 17:57:51 -0400

    added 404 page content

--- waterloop-site/src/pages/404.tsx
+++ waterloop-site/src/pages/404.tsx
@@ -1,8 +1,19 @@
+import React from "react";
+import "../theme/styles.scss";
+import Goose from "../static/img/404page/errorgoose.png"; 
+
 
 const ErrorPage: React.FC = () => (
-    <div>
-        <h1>Hello</h1>
+    <div className="errorpage">
+        <div className="errorpagetext">
+        <h3>ERROR</h3>
+        <h1>404</h1>
+        <p>Oops! The page you are looking for was not found.</p>
+        </div>
+        <div className="errorpageimage">
+        <img className="errorgoose" src={Goose}></img>
+        </div>
     </div>
-)
+); 
 
 export default ErrorPage;
