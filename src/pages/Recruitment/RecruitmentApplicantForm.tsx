218564081a7f04a2db77683b1cc65da2ad78d396
Lavan Sumanan <lavan.sumanan@gmail.com>
2021-11-19 20:19:38 -0500

    Created FileUpload component
    t

--- src/pages/Recruitment/RecruitmentApplicantForm.tsx
+++ src/pages/Recruitment/RecruitmentApplicantForm.tsx
@@ -2,17 +2,53 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import TextInput from "../../components/TextInput";
 import RadioButton from "components/RecruitmentForm/RadioButton";
+import FileUpload from "components/FileUpload";
 //import DropDownList from "../../components/DropDownList";
 import { Button } from "../../components/Button";
 //import useRecruitmentForm from '../../hooks/recruitment-form';
 //import { useRouteMatch } from 'react-router-dom';
 
-const Wrapper = styled.div`
-  margin: 3em 4em 3em 4em;
+const FormWrapper = styled.div`
+  margin: 3em 5em;
+`;
+
+const SectionHeader = styled.h3`
+  text-align: left;
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 47px;
+  color: #000000;
+`;
+
+const QuestionHeader = styled.h4`
+  text-align: left;
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  color: #000000;
+`;
+
+const RadioText = styled.h4`
+  margin: auto;
+  text-align: left;
+  font-family: IBM Plex Sans;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  color: #000000;
+`;
+
+const RightAlignedContainer = styled.div`
+  display: flex
+  align-items: flex-end;
 `;
 
 const SubmitButton = styled(Button)`
   width: 100px;
+  margin-top: 3em;
 `;
 
 const RecruitmentApplicantForm: React.FC = () => {
@@ -27,11 +63,11 @@ const RecruitmentApplicantForm: React.FC = () => {
 
   } = useRecruitmentForm(positionId); */
   return (
-    <div>
-      <p>&lt; Back</p>
-      <Wrapper>
-        <h3>Contact Info</h3>
-        <h4>Full name</h4>
+    <FormWrapper>
+      <p onClick={() => {}}>&lt; Back</p>
+      <FormWrapper>
+        <SectionHeader>Contact Info</SectionHeader>
+        <QuestionHeader>Full name</QuestionHeader>
         <TextInput
           className="first-name"
           multiLine={false}
@@ -43,21 +79,53 @@ const RecruitmentApplicantForm: React.FC = () => {
           requiredText="This is a required field"
           isError={false /* CHANGE THIS TO HOOK STUFF */}
         />
-        <h4>Email</h4>
-        <input id="email" type="email" placeholder="Email address"></input>
-      </Wrapper>
-      <SubmitButton
-        onClick={
-          () =>
-            window.open(
-              "http://wloop.ca/subscribe"
-            ) /*function from hook that posts to api after validating input*/
-        }
-        text="Submit"
-        backgroundColor="yellow"
-        textColor="black"
-      />
-    </div>
+
+        <QuestionHeader>
+          Are you living in Waterloo and willing to participate in in-person
+          work?
+        </QuestionHeader>
+        <RadioText>
+          <RadioButton
+            checked={false}
+            onChange={() => {}}
+            name="Yes"
+            question="inPerson"
+          ></RadioButton>
+        </RadioText>
+        <RadioText>
+          <RadioButton
+            checked={false}
+            onChange={() => {}}
+            name="No"
+            question="inPerson"
+          ></RadioButton>
+        </RadioText>
+
+        <QuestionHeader>
+          Please attach your Resume and other Documents
+        </QuestionHeader>
+        <FileUpload
+          name="resume-docs"
+          onChange={() => {
+            console.log("file change");
+          }}
+          multiple={false}
+        />
+        <RightAlignedContainer>
+          <SubmitButton
+            onClick={
+              () =>
+                window.open(
+                  "http://wloop.ca/subscribe"
+                ) /*function from hook that posts to api after validating input*/
+            }
+            text="Submit"
+            backgroundColor="yellow"
+            textColor="black"
+          />
+        </RightAlignedContainer>
+      </FormWrapper>
+    </FormWrapper>
   );
 };
 
