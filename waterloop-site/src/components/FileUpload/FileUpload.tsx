9a45b32c4a51bc799b301f64064ce0306a97ff94
Lavan Sumanan <lavan.sumanan@gmail.com>
2021-11-27 17:17:40 -0500

    Fixed styling for components in the recruitment form

--- waterloop-site/src/components/FileUpload/FileUpload.tsx
+++ waterloop-site/src/components/FileUpload/FileUpload.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 const FileUpload = ({
   name,
+  value,
   onChange,
   multiple,
 }: {
   name: string;
+  value: string;
   onChange: Function;
   multiple: boolean;
 }) => {
@@ -27,7 +29,7 @@ const FileUpload = ({
     text-align: center;
     color: rgba(0, 0, 0, 0.5);
     width: 100%;
-    border: 1.2px dashed #000000;
+    border: 1.2px dashed #c4c4c4;
     box-sizing: border-box;
     border-radius: 6px;
     background: #ffffff;
@@ -47,7 +49,7 @@ const FileUpload = ({
             <path
               d="M9.08325 32.75H14.9166V15.25H23.6666L11.9999 0.666687L0.333252 15.25H9.08325V32.75Z"
               fill="black"
-              fill-opacity="0.4"
+              fillOpacity="0.4"
             />
           </svg>
         </span>
@@ -62,7 +64,7 @@ const FileUpload = ({
             <path
               d="M53.3333 20.5H6.66659V0.083313H0.833252V20.5C0.833252 23.7171 3.4495 26.3333 6.66659 26.3333H53.3333C56.5503 26.3333 59.1666 23.7171 59.1666 20.5V0.083313H53.3333V20.5Z"
               fill="black"
-              fill-opacity="0.4"
+              fillOpacity="0.4"
             />
           </svg>
         </span>
@@ -77,6 +79,7 @@ const FileUpload = ({
         onChange={(e) => onChange(e.target.value)}
         multiple={multiple}
       />
+      {value && <p>{value}</p>}
     </div>
   );
 };
