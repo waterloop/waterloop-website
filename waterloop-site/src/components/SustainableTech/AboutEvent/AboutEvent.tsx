06cc92ccff9285b79f368e745293a8933554ad37
Preyansh <preyanshkachhiapk@gmail.com>
2022-02-15 23:45:03 -0500

    Created the skeleton jsx for the About Event section

--- waterloop-site/src/components/SustainableTech/AboutEvent/AboutEvent.tsx
+++ waterloop-site/src/components/SustainableTech/AboutEvent/AboutEvent.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
 
-const AboutEvent: React.FC = () => <div>About Event</div>;
+const AboutEvent: React.FC = () => (
+  <div>
+    {/* Header Container */}
+    <div>
+      <h2>About The Event</h2>
+    </div>
+    {/* Details Container - contains all details*/}
+    <div>
+      {/* you can do a map function to render the 
+            following card component for each detail */}
+      <div>
+        <div>Image</div>
+        <p>Details Text</p>
+      </div>
+    </div>
+    {/* Teams header Container */}
+    <div>
+      <h2>Teams</h2>
+    </div>
+
+    {/* Teams Details Container - rectangular container for teams details */}
+    <div>
+      <div>Image</div>
+      <p>Teams Text</p>
+    </div>
+
+    <div>
+      {/* you can do a map function to render the 
+            following card component */}
+      <div>Rectangle container</div>
+    </div>
+  </div>
+);
 
 export default AboutEvent;
