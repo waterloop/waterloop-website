b1fac55cd4b9e6348ae88a28cc69559fb4cfc92b
unknown <evanmatt.wong@gmail.com>
2022-02-20 11:19:07 -0500

    added in main Footer as STE Footer per designs

--- waterloop-site/src/components/SustainableTech/Footer/STEFooter.tsx
+++ waterloop-site/src/components/SustainableTech/Footer/STEFooter.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import Footer from '../../../sections/Footer/Footer';
 
-const STEFooter: React.FC = () => <div>STE Footer</div>;
+const STEFooter: React.FC = () => <Footer/>;
 
 export default STEFooter;
