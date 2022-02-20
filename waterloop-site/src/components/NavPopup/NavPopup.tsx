21db46d3be551e9aa11c01abf49e6ceb7fa33ec8
Vanessa Mac <vanessavaleriemac@gmail.com>
2022-02-20 12:49:13 -0500

    Add fade-in animation on popup load

--- waterloop-site/src/components/NavPopup/NavPopup.tsx
+++ waterloop-site/src/components/NavPopup/NavPopup.tsx
@@ -8,10 +8,44 @@ const Card = styled.div`
     padding: 20px;
     background: #F4F4F4;
     width: 330px;
-    height: 170px;    
+    height: 165px;    
     border-radius: 6px;
     z-index: 100000;
     align-items: center;
+    font-size: 15px;
+    opacity: 0;
+    animation: fadeIn 5s;
+    -webkit-animation: fadeIn 2s;
+    -moz-animation: fadeIn 2s;
+    -o-animation: fadeIn 2s;
+    -ms-animation: fadeIn 2s;
+    animation-delay: 0.5s;
+    animation-fill-mode: forwards;
+
+    @keyframes fadeIn {
+        0% {opacity:0;}
+        100% {opacity:1;}
+      }
+      
+      @-moz-keyframes fadeIn {
+        0% {opacity:0;}
+        100% {opacity:1;}
+      }
+      
+      @-webkit-keyframes fadeIn {
+        0% {opacity:0;}
+        100% {opacity:1;}
+      }
+      
+      @-o-keyframes fadeIn {
+        0% {opacity:0;}
+        100% {opacity:1;}
+      }
+      
+      @-ms-keyframes fadeIn {
+        0% {opacity:0;}
+        100% {opacity:1;}
+      }
 `;
 
 const Title = styled.div`
@@ -44,6 +78,10 @@ const Button = styled.button`
     border-radius: 100px;
     padding: 8px 16px;
     margin-left: 30%;
+    &:hover {
+        background: #3E6232;
+        transition: background-color 0.2s ease-in;
+    }
 `;
 
 
@@ -60,7 +98,7 @@ const NavPopup: React.FC = () => {
             <Title>Sustainable Tech Event</Title>
             <CloseIcon onClick={closePopup}/>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            <Button onClick={(): void => console.log('submitting form')}>Check it out!</Button>
+            <Button onClick={(): Window | null => window.open("/sustainable-tech", "_self")}>Check it out!</Button>
         </Card>
     );
 };
