6729ce7b83b2ff8950c39d3bbd3e2d15b8e6a087
Vanessa Mac <vanessavaleriemac@gmail.com>
2022-02-21 03:18:17 -0500

    Add smooth accordian expansion transition

--- waterloop-site/src/components/SustainableTech/FAQ/AccordionDropdown.tsx
+++ waterloop-site/src/components/SustainableTech/FAQ/AccordionDropdown.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled, { keyframes } from 'styled-components';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 
 interface Props {
   open?: boolean;
@@ -8,7 +8,7 @@ interface Props {
 }
 
 const rotateIcon = keyframes`
-  50% {transform: rotate(-180deg);}
+  50% {transform: rotate(180deg);}
 `;
 
 const Question = styled.div`
@@ -17,11 +17,11 @@ const Question = styled.div`
   background: #3e6232;
   border-radius: 8px;
   padding: 20px 65px 20px 20px;
-  margin-top: 15px;
+  margin-top: 20px;
   position: relative;
 `;
 
-export const ExpandArrow = styled(ExpandMoreIcon)`
+export const ExpandArrow = styled(ExpandLessIcon)`
   color: white;
   position: absolute;
   right: 20px;
@@ -30,12 +30,18 @@ export const ExpandArrow = styled(ExpandMoreIcon)`
   animation: ${rotateIcon} linear;
 `;
 
-const Answer = styled.div`
+const Answer = styled.div<Props>`
   font-size: 18px;
   background: #daf4d1;
-  padding: 30px 20px 20px 20px;
-  margin-top: -10px;
   border-radius: 0px 0px 8px 8px;
+  margin-top: -10px;
+  overflow: hidden;
+  max-height: ${props => props.open ? "1000px" : "0"};
+  transition: max-height 0.8s cubic-bezier(0.25, 0.1, 0.25, 1.0);
+`;
+
+const AnswerText = styled.p`
+  padding: 20px 20px 10px 20px;
 `;
 
 const AccordionDropdown: React.FC<Props> = ({ open, children, title }) => {
@@ -52,12 +58,14 @@ const AccordionDropdown: React.FC<Props> = ({ open, children, title }) => {
         <ExpandArrow
           fontSize="large"
           style={{
-            transition: 'transform .5s',
-            transform: `rotate(${isOpen ? 0 : -180}deg)`,
+            transition: 'transform 0.8s cubic-bezier(0.25, 0.1, 0.25, 1.0)',
+            transform: `rotate(${isOpen ? 0 : 180}deg)`,
           }}
         />
       </Question>
-      {isOpen && <Answer>{children}</Answer>}
+      <Answer open={isOpen} title="">
+        <AnswerText>{children}</AnswerText>
+      </Answer>
     </div>
   );
 };
