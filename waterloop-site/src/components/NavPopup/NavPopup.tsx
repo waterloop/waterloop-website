50fb23d229fa93f73baaae0abe05f165da444b80
Vanessa Mac <vanessavaleriemac@gmail.com>
2022-02-21 03:39:10 -0500

    Remove @styled-icons/evil dependency and use @material-ui/icons instead

--- waterloop-site/src/components/NavPopup/NavPopup.tsx
+++ waterloop-site/src/components/NavPopup/NavPopup.tsx
@@ -1,40 +1,44 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import {Close} from '@styled-icons/evil';
+import CloseIcon from '@material-ui/icons/Close';
 
 const Card = styled.div`
-    position: absolute;
-    right: 20px;
-    padding: 20px;
-    background: #F4F4F4;
-    width: 300px;
-    height: 165px;    
-    border-radius: 6px;
-    z-index: 100000;
-    align-items: center;
-    font-size: 15px;
-    opacity: 0;
+  position: absolute;
+  right: 20px;
+  padding: 20px;
+  background: #f4f4f4;
+  width: 300px;
+  height: 165px;
+  border-radius: 6px;
+  z-index: 100000;
+  align-items: center;
+  font-size: 15px;
+  opacity: 0;
 
-    animation: fadeIn 2.5s;
-    animation-delay: 0.3s;
-    animation-fill-mode: forwards;
-    @keyframes fadeIn {
-      0% {opacity:0;}
-      100% {opacity:1;}
+  animation: fadeIn 2.5s;
+  animation-delay: 0.3s;
+  animation-fill-mode: forwards;
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
     }
+    100% {
+      opacity: 1;
+    }
+  }
 `;
 
 const Title = styled.div`
-    font-size: 24px;
-    margin-bottom: 15px;
+  font-size: 24px;
+  margin-bottom: 15px;
 `;
 
-export const CloseIcon = styled(Close)`
+export const ClosePopupIcon = styled(CloseIcon)`
   color: black;
   width: 30px;
   position: absolute;
-  right: 20px;
-  top: 20px;
+  right: 18px;
+  top: 18px;
   &:hover {
     color: grey;
     cursor: pointer;
@@ -42,42 +46,49 @@ export const CloseIcon = styled(Close)`
 `;
 
 const Button = styled.button`
-    margin-top: 20px;
-    margin-left: auto;
-    margin-right: auto;
-    text-align: center;
-    height: 37px;
-    font-weight: bold;
-    font-size: 16px;
-    color: white;
-    display: block;
-    background: #84B274;
-    border: none;
-    border-radius: 100px;
-    padding: 8px 16px;
-    &:hover {
-        background: #3E6232;
-        transition: background-color 0.2s ease-in;
-    }
+  margin-top: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  height: 37px;
+  font-weight: bold;
+  font-size: 16px;
+  color: white;
+  display: block;
+  background: #84b274;
+  border: none;
+  border-radius: 100px;
+  padding: 8px 16px;
+  &:hover {
+    cursor: pointer;
+    background: #3e6232;
+    transition: background-color 0.2s ease-in;
+  }
 `;
 
-
 const NavPopup: React.FC = () => {
-    const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(true);
 
-    function closePopup() {
-        setIsOpen(false);
-    }
+  function closePopup() {
+    setIsOpen(false);
+  }
 
-    if (!isOpen) { return null; }
-    return (
-        <Card>
-            <Title>Sustainable Tech Event</Title>
-            <CloseIcon onClick={closePopup}/>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            <Button onClick={(): Window | null => window.open("/sustainable-tech", "_self")}>Check it out!</Button>
-        </Card>
-    );
+  if (!isOpen) {
+    return null;
+  }
+  return (
+    <Card>
+      <Title>Sustainable Tech Event</Title>
+      <ClosePopupIcon fontSize="large" onClick={closePopup} />
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor
+      sit amet, consectetur adipiscing elit.
+      <Button
+        onClick={(): Window | null => window.open('/sustainable-tech', '_self')}
+      >
+        Check it out!
+      </Button>
+    </Card>
+  );
 };
 
 export default NavPopup;
--- waterloop-site/src/pages/Home.tsx
+++ waterloop-site/src/pages/Home.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { NavPopup } from "components/NavPopup";
-import Hero from "components/Hero/Landing";
-import { Sponsors } from "components/SponsorsLanding";
-import { ImgJSON } from "components/SponsorsLanding";
-import { TextWithImage, MyProps } from "components/TextWithImage";
-import { ColumnBlock, Props } from "components/ColumnBlock";
-import Text from "static/copy/Landing/TextWithImage.json";
-import ColText from "static/copy/Landing/ColumnBlock.json";
-import "../theme/styles.scss";
+import React from 'react';
+import { NavPopup } from 'components/NavPopup';
+import Hero from 'components/Hero/Landing';
+import { Sponsors } from 'components/SponsorsLanding';
+import { ImgJSON } from 'components/SponsorsLanding';
+import { TextWithImage, MyProps } from 'components/TextWithImage';
+import { ColumnBlock, Props } from 'components/ColumnBlock';
+import Text from 'static/copy/Landing/TextWithImage.json';
+import ColText from 'static/copy/Landing/ColumnBlock.json';
+import '../theme/styles.scss';
 // Images for sponsors:
-import Brent from "../static/img/official-sponsors/BrentsWeldingAndFab.png";
-import Weef from "../static/img/official-sponsors/weef.jpg";
-import WaterlooEng from "../static/img/official-sponsors/wes.jpg";
-import UW from "../static/img/official-sponsors/foe.jpg";
-import ClickUp from "../static/img/official-sponsors/ClickUp.png";
-import WCBranham from "../static/img/official-sponsors/WCBranham.png";
+import Brent from '../static/img/official-sponsors/BrentsWeldingAndFab.png';
+import Weef from '../static/img/official-sponsors/weef.jpg';
+import WaterlooEng from '../static/img/official-sponsors/wes.jpg';
+import UW from '../static/img/official-sponsors/foe.jpg';
+import ClickUp from '../static/img/official-sponsors/ClickUp.png';
+import WCBranham from '../static/img/official-sponsors/WCBranham.png';
 
-import Goals from "../static/img/landing/textwithimage/goals.png";
-import Competition from "../static/img/landing/textwithimage/competition.png";
+import Goals from '../static/img/landing/textwithimage/goals.png';
+import Competition from '../static/img/landing/textwithimage/competition.png';
 
 const castData: MyProps[] = Text;
 const imgData: string[] = [Goals, Competition];
@@ -25,24 +25,24 @@ const ColData: Props[] = ColText;
 
 // Place data here:
 const imgJSON: ImgJSON[] = [
-  { imgSrc: Brent, imgAlt: "Brent" },
-  { imgSrc: Weef, imgAlt: "Weef" },
-  { imgSrc: WaterlooEng, imgAlt: "WaterlooEng" },
-  { imgSrc: UW, imgAlt: "UW" },
-  { imgSrc: ClickUp, imgAlt: "ClickUp" },
-  { imgSrc: WCBranham, imgAlt: "WCBranham" },
+  { imgSrc: Brent, imgAlt: 'Brent' },
+  { imgSrc: Weef, imgAlt: 'Weef' },
+  { imgSrc: WaterlooEng, imgAlt: 'WaterlooEng' },
+  { imgSrc: UW, imgAlt: 'UW' },
+  { imgSrc: ClickUp, imgAlt: 'ClickUp' },
+  { imgSrc: WCBranham, imgAlt: 'WCBranham' },
 ];
 
 const Home: React.FC = () => (
   <div>
-    <NavPopup/>
+    <NavPopup />
     <Hero description="A faster look on the future." anchor="#home"></Hero>
 
     <div className="pageContainer">
-      <div id={"#home"}></div>
+      <div id={'#home'}></div>
       <TextWithImage
         data={castData}
-        textPos={"alternate"}
+        textPos={'alternate'}
         imgData={imgData}
       ></TextWithImage>
     </div>
