2099220d92626cee7ce68fd93234ce19448c1179
Samantha Grieco <samtb1@outlook.com>
2022-02-26 21:55:53 -0500

    changed logo

--- waterloop-site/src/components/SustainableTech/Hero/Hero.tsx
+++ waterloop-site/src/components/SustainableTech/Hero/Hero.tsx
@@ -19,27 +19,63 @@ const LandingContent = styled.div`
 
 const LandingImageContainer = styled.img`
   height: 100vh;
+  overflow: hidden;
   background-color: #E5F6FA;
 `;
 
 const MainContent = styled.div`
   position: absolute;
-  background-color: black;
-  top: 35%;
+  top: 45%;
   left: 50%;
   transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: fit-content;
 `;
 
 const GreenTech = styled.img`
   height: 300px;
   width: auto;
+
+  @media (max-width: 550px) {
+    height: 190px;
+  }
+`;
+
+const ButtonContainer = styled.button`
+  border-radius: 30px;
+  background-color: #203d7a;
+  text-decoration: none;
+  color: white;
+  font-family: 'IBM Plex Sans';
+  font-size: 24px;
+  font-weight: 500;
+  padding: 15px 20%;
+  border: #203d7a;
+  margin: 50px 0;
+  transition: color 0.1s ease;
+  transition: background-color 0.1s ease;
+
+  &:hover {
+    cursor: pointer;
+    color: #203d7a;
+    background-color: white;
+  }
+
+  @media (max-width: 550px) {
+    font-size: 17px;
+    padding: 10px 15%;
+  }
 `;
 
 const Hero: React.FC = () => 
 <LandingContainer>
   <LandingContent>
-      <MainContent>
+      <MainContent id="home-scroll">
         <GreenTech alt={'Green Tech Logo'} src={GreenTechLogo}></GreenTech>
+        <ButtonContainer as="a" href="http://Google_Form_Here">Sign Up</ButtonContainer>
       </MainContent>
      <LandingImageContainer alt={'Green Tech Landing Image'} src={LandingImage} />
   </LandingContent>
