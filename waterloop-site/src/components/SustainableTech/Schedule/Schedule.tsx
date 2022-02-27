3e60156c7648e410e6a82f826b4f8620cb5c61f5
William Park <william.park@uwaterloo.ca>
2022-02-27 12:18:34 -0800

    Add background image to Schedule section

--- waterloop-site/src/components/SustainableTech/Schedule/Schedule.tsx
+++ waterloop-site/src/components/SustainableTech/Schedule/Schedule.tsx
@@ -1,48 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import BackgroundSVG from 'static/img/sustainable-tech/schedule-background.svg';
 import Agenda from './Agenda';
 
 const Container = styled.div`
-  background-image: linear-gradient(#a3c7e1, #ffffff);
+  background-image: linear-gradient(#ffffff, #e7f1e4);
   overflow-x: hidden;
-  padding: 100px 0;
+  padding: 100px 0 200px 0;
+  position: relative;
+`;
+
+const BackgroundImg = styled.img.attrs({
+  src: `${BackgroundSVG}`,
+})`
+  width: 100%;
+  position: absolute;
+  bottom: 0%;
 `;
 
 const Title = styled.h1`
-  font-size: 44px;
+  font-size: 48px;
   text-align: center;
   color: #514c49;
   width: 100%;
-  margin-bottom: 30px;
-`;
-
-const Headline = styled.h2`
-  font-size: 24px;
-  text-align: center;
-  color: #555555;
-  width: 100%;
-  margin-bottom: 30px;
-  font-style: normal;
-`;
-
-const Text = styled.p`
-  color: #555555;
-  width: 60%;
-  margin: auto;
+  margin-bottom: 50px;
 `;
 
 const Schedule: React.FC = () => (
   <Container>
     <Title>SCHEDULE</Title>
-    <Headline>SUBTEXT / HEADLINE</Headline>
-    <Text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus
-      eros nec eros tempus, ac euismod eros elementum. Nullam eu vehicula enim,
-      at sollicitudin urna. Ut sed augue non nisi tincidunt pulvinar. Nulla
-      facilisi.
-    </Text>
     <Agenda />
+    <BackgroundImg />
   </Container>
 );
 
