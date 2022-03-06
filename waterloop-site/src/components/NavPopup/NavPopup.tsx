d56bdafcd62adf6d852ccc35b9a9969cc06e1b37
Vanessa Mac <vanessavaleriemac@gmail.com>
2022-03-06 04:10:30 -0500

    Fix cursor to pointer on Button hover

--- waterloop-site/src/components/NavPopup/NavPopup.tsx
+++ waterloop-site/src/components/NavPopup/NavPopup.tsx
@@ -69,6 +69,7 @@ const Button = styled.button`
 
 const StyledLink = styled(Link)`
   text-decoration: none;
+  color: white;
 `;
 
 const NavPopup: React.FC = () => {
@@ -87,9 +88,9 @@ const NavPopup: React.FC = () => {
       <ClosePopupIcon fontSize="large" onClick={closePopup} />
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor
       sit amet, consectetur adipiscing elit.
-      <StyledLink to="/sustainable-tech">
-        <Button>Check it out!</Button>
-      </StyledLink>
+      <Button>
+        <StyledLink to="/sustainable-tech">Check it out!</StyledLink>
+      </Button>
     </Card>
   );
 };
