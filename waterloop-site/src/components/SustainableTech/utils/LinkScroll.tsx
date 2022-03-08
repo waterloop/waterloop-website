8577a0158fb124c5ab602bf9d40564016d3dff2c
William Park <william.park@uwaterloo.ca>
2022-03-08 01:39:43 -0800

    Refactor scrollTo util to take navbar height into consideration

--- waterloop-site/src/components/SustainableTech/utils/LinkScroll.tsx
+++ waterloop-site/src/components/SustainableTech/utils/LinkScroll.tsx
@@ -1,9 +1,12 @@
 // Moves user to the desired section of the webpage
 const scrollTo = (id: string) => {
+  const yOffset = -64; // We want to offset by the height of the navbar: 64px
   const element = document.getElementById(id);
   if (!(element === null)) {
-    element.scrollIntoView({ block: 'center', behavior: 'smooth' });
+    const y =
+      element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    window.scrollTo({ top: y, behavior: 'smooth' });
   }
-}
+};
 
 export default scrollTo;
