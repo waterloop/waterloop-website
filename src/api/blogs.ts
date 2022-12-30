d2ab53913d204c683aed087bb50491b04d3abe65
Lavan Sumanan <lavan.sumanan@gmail.com>
2022-12-30 16:25:54 -0500

    update getBlog urls for FE api

--- src/api/blogs.ts
+++ src/api/blogs.ts
@@ -2,8 +2,8 @@ import { Server } from "server";
 import { AxiosResponse } from "axios";
 import { PostResponse } from "blogs";
 
-const getBlogPosts = (server: Server) => (): Promise<AxiosResponse<PostResponse>> => server.get(`blogs`);
-const getLatestBlogs = (server: Server) => (): Promise<AxiosResponse<PostResponse>> => server.get(`blogs/latest`);
+const getBlogPosts = (server: Server) => (): Promise<AxiosResponse<PostResponse>> => server.get(`/api/blogs`);
+const getLatestBlogs = (server: Server) => (): Promise<AxiosResponse<PostResponse>> => server.get(`/api/blogs/latest`);
 
 export default (server: Server) => ({
     getBlogPosts: getBlogPosts(server),
