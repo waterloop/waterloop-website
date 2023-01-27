7214b967a70487965316c4e088313d2ec6c60c66
sashco mistelbacher <sashco.mistelbacher@gmail.com>
2023-01-26 20:32:24 -0500

    remove commented code

--- src/pages/Recruitment/RecruitmentApplicantForm.tsx
+++ src/pages/Recruitment/RecruitmentApplicantForm.tsx
@@ -1,58 +1,8 @@
 import React, { useCallback } from 'react';
 import styled from 'styled-components';
-// import TextInput from "../../components/TextInput";
-// import RadioButton from "components/RecruitmentForm/RadioButton";
-// import FileUpload from "components/FileUpload";
-//import DropDownList from "../../components/DropDownList";
-// import { Button } from "../../components/Button";
-// //import useRecruitmentForm from '../../hooks/recruitment-form';
-//import { useRouteMatch } from 'react-router-dom';
 import { useParams, useHistory } from 'react-router';
 import { RecruitmentForm } from '../../components/RecruitmentForm';
 import usePostingPostingById from 'hooks/posting-by-id';
-
-// const FormWrapper = styled.div`
-//   margin: 3em 5em;
-// `;
-
-// const SectionHeader = styled.h3`
-//   text-align: left;
-//   font-family: IBM Plex Sans;
-//   font-style: normal;
-//   font-weight: 600;
-//   font-size: 36px;
-//   line-height: 47px;
-//   color: #000000;
-// `;
-
-// const QuestionHeader = styled.h4`
-//   text-align: left;
-//   font-family: IBM Plex Sans;
-//   font-style: normal;
-//   font-weight: 600;
-//   font-size: 24px;
-//   color: #000000;
-// `;
-
-// const RadioText = styled.h4`
-//   margin: auto;
-//   text-align: left;
-//   font-family: IBM Plex Sans;
-//   font-style: normal;
-//   font-weight: 600;
-//   font-size: 24px;
-//   color: #000000;
-// `;
-
-// const RightAlignedContainer = styled.div`
-//   display: flex
-//   align-items: flex-end;
-// `;
-
-// const SubmitButton = styled(Button)`
-//   width: 100px;
-//   margin-top: 3em;
-// `;
 
 const BackButton = styled.p`
   cursor: pointer;
