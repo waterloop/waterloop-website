6277dbeba3bcb2e9cc521be458aa7f21866aa0d6
Mark123M <mark123ymm@gmail.com>
2023-02-12 00:17:11 -0500

    fetching geese info from cms

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,10 +3,12 @@ import postings from './postings';
 import server from './server';
 import sponsors from './sponsors';
 import teams from './teams';
+import geeseInfo from './geese-info';
 
 export default {
   postings: postings(server),
   teams: teams(server),
   sponsors: sponsors(server),
-  newsletter: newsletter(server)
+  newsletter: newsletter(server),
+  geeseInfo: geeseInfo(server)
 };
